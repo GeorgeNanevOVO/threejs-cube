00e09ce495e5cd3ada9d6826522113e96fa44bd0
GeorgeNanevOVO <georgi.nanev@ovoenergy.com>
2021-04-28 01:09:59 +0300

    changed drei dependency library. Added ambientlight and orbit controls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import "./index.css";
+
+const Box = () => {
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry"></boxBufferGeometry>
+      <meshLambertMaterial attach="material" color="grey"></meshLambertMaterial>
+    </mesh>
+  );
+};
+
 const App = () => {
-  return <Canvas></Canvas>;
+  return (
+    <Canvas>
+      <OrbitControls></OrbitControls>
+      <ambientLight intensity={0.8}></ambientLight>
+      <Box></Box>
+    </Canvas>
+  );
 };
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
---
+++ src/components/Box.js
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// export default function Box() {
+//   return (
+//     <>
+//       <OrbitControls></OrbitControls>
+//       <mesh>
+//         <boxBufferGeometry attach="geometry"></boxBufferGeometry>
+//         <meshLambertMaterial
+//           attach="material"
+//           color="blue"
+//         ></meshLambertMaterial>
+//       </mesh>
+//     </>
+//   );
+// }
