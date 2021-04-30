2c3631f218f8db92211621e96310f043efcaf188
GeorgeNanevOVO <georgi.nanev@ovoenergy.com>
2021-04-30 22:02:37 +0300

    added background stars and spotlight

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stars } from "@react-three/drei";
 import "./index.css";
 
 const Box = () => {
@@ -17,7 +17,9 @@ const App = () => {
   return (
     <Canvas>
       <OrbitControls></OrbitControls>
+      <Stars></Stars>
       <ambientLight intensity={0.8}></ambientLight>
+      <spotLight position={[9, 15, 11]} angle={0.3}></spotLight>
       <Box></Box>
     </Canvas>
   );
